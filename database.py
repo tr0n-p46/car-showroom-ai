a23679cc8d4b5bd5a608e3de15409e30e3ec9d54
Souvik Das <souvikd197@gmail.com>
2026-04-02 07:36:19 +0530

    Update database.py

--- database.py
+++ database.py
@@ -8,8 +8,7 @@ load_dotenv()
 url: str = "https://kfwtxlswenleozteuzac.supabase.co"
 
 # This pulls from Railway's environment variables
-supabase_key = 
-os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtmd3R4bHN3ZW5sZW96dGV1emFjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTA3NzEzNSwiZXhwIjoyMDkwNjUzMTM1fQ.bWut3sXXVJicDqeqJ6-zSdQxo8tk45M4jZOsGeeueLw")
+supabase_key = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtmd3R4bHN3ZW5sZW96dGV1emFjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTA3NzEzNSwiZXhwIjoyMDkwNjUzMTM1fQ.bWut3sXXVJicDqeqJ6-zSdQxo8tk45M4jZOsGeeueLw")
 
 if not supabase_key:
     raise ValueError("SUPABASE_KEY environment variable is not set!")
