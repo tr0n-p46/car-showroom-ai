a7392fca6ce2684c5b15da504724bcd6ebab61ac
Souvik Das <souvikd197@gmail.com>
2026-04-02 07:34:23 +0530

    Fixed syntax errors

--- database.py
+++ database.py
@@ -4,9 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Your specific Supabase URL
 url: str = "https://kfwtxlswenleozteuzac.supabase.co"
-key: str = 
-os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtmd3R4bHN3ZW5sZW96dGV1emFjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTA3NzEzNSwiZXhwIjoyMDkwNjUzMTM1fQ.bWut3sXXVJicDqeqJ6-zSdQxo8tk45M4jZOsGeeueLw") 
-# Get 'service_role' key from 
-Supabase settings
-supabase: Client = create_client(url, key)
+
+# This pulls from Railway's environment variables
+supabase_key = 
+os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtmd3R4bHN3ZW5sZW96dGV1emFjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTA3NzEzNSwiZXhwIjoyMDkwNjUzMTM1fQ.bWut3sXXVJicDqeqJ6-zSdQxo8tk45M4jZOsGeeueLw")
+
+if not supabase_key:
+    raise ValueError("SUPABASE_KEY environment variable is not set!")
+
+supabase: Client = create_client(url, supabase_key)
